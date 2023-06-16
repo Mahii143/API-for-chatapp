356e5a3dfaa6569ef3884bd319bb0e746b1f2d6c
Mahii143 <2012091@nec.edu.in>
2023-06-16 15:19:11 +0530

    log user

--- server.js
+++ server.js
@@ -427,6 +427,8 @@ app.post("/join-channel", authenticateToken, async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username } = req.body;
+  
+  console.log(req.body);
   const authorisedUser = users.find((u) => u.name === username);
   // console.log(authorisedUser);
   if (!authorisedUser) {
